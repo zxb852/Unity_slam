a35dbf300c6929be2a6b15435a3f2fc5f3b1e389
ZXB852 <zxb852@sina.com>
2019-07-18 16:34:47 +0800

    添加了静态小地图功能

---
+++ Unity_slam/Assets/WebCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCam : MonoBehaviour
+{
+    public string deviceName;
+    WebCamTexture camTexture;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+            deviceName = devices[0].name;
+            camTexture = new WebCamTexture(deviceName, 800, 600, 12);
+            transform.GetComponent<MeshRenderer>().material.mainTexture = camTexture;
+            camTexture.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
