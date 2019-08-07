c65039894b6c5b318e087238a62c79c677de50e2
ZXB852 <zxb852@sina.com>
2019-08-07 16:43:56 +0800

    增加了 画线 和 鼠标响应功能

---
+++ Unity_slam/Assets/Minimap_ctrl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Minimap_ctrl : MonoBehaviour
+{
+    public GameObject Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //划线的例程（假设地图分辨率为1000*1000）：
+        Vector3 orgPos = new Vector3(0, 0);
+        Vector3 endPos = new Vector3(500, 500);
+        Player.GetComponent<Minimap_draw>().GLDrawLine(orgPos, endPos);
+
+
+        //鼠标响应的例程
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Input.mousePosition);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("mouse button up");
+        }
+    }
+
+
+
+}
---
+++ Unity_slam/Assets/Minimap_draw.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joint
+{
+    public Vector3 org;
+    public Vector3 end;
+}
+
+public class Minimap_draw : MonoBehaviour
+{
+    private Vector3 orgPos;
+    private Vector3 endPos;
+    private ArrayList posAL;
+    public Material lineMaterial;
+    public int width = 1000;
+    public int height = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posAL = new ArrayList();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void GLDrawLine(Vector3 beg, Vector3 end)
+    {
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        beg.x = beg.x/ width ;
+        end.x = end.x / width;
+        beg.y = beg.y / height;
+        end.y = end.y / height;
+        joint tmpJoint = new joint();
+        tmpJoint.org = beg;
+        tmpJoint.end = end;
+
+        posAL.Add(tmpJoint);
+
+        lineMaterial.SetPass(0);
+        GL.Begin(GL.LINES);
+        GL.Color(new Color(255, 0, 0, 0.5f));
+        for (int i = 0; i < posAL.Count; i++)
+        {
+            joint tj = (joint)posAL[i];
+            Vector3 tmpBeg = tj.org;
+            Vector3 tmpEnd = tj.end;
+            GL.Vertex3(tmpBeg.x, tmpBeg.y, tmpBeg.z);
+            GL.Vertex3(tmpEnd.x, tmpEnd.y, tmpEnd.z);
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+    public void ClearLines()
+    {
+        posAL.Clear();
+    }
+
+
+
+    void OnPostRender()
+    {
+        GLDrawLine(orgPos, endPos);
+    }
+}
